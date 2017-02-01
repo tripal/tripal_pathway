5d41e6be112b205ee59bbae8cf40689465e261be
kentnf <kentnf@gmail.com>
2017-02-01 17:39:21 -0500

    add help info

--- theme/pwy_tool_help.tpl.php
+++ theme/pwy_tool_help.tpl.php
@@ -5,25 +5,34 @@
 ?>
 
 <h3>Module Description</h3>
-<p>This module provides a basic interface to allow your users to perform go enrichment analysis.</p>
+<p>This module provides a basic interface to allow your users to perform pathway enrichment analysis.</p>
 
 <h3>Setup Instructions</h3>
 <ol>
-<li>Install NCBI BLAST+ on your server (Tested with 2.2.26+). There is a <a href="https://launchpad.net/ubuntu/+source/ncbi-blast+">package available for Ubuntu</a> to ease installation. Optionally you can set the path to your BLAST executable <a href="<?php print url('admin/tripal/extension/tripal_blast/blast_ui');?>">in the settings</a>.</li>
-<li>Optionally, create Tripal External Database References to allow you to link the records in your BLAST database to further information. To do this simply go to <a href="<?php print url('admin/tripal/chado/tripal_db/add'); ?>" target="_blank">Tripal > Chado Modules > Databases > Add DB</a> and make sure to fill in the Database prefix which will be concatenated with the record IDs in your BLAST database to determine the link-out to additional information. Note that a regular expression can be used when creating the BLAST database to indicate what the ID is.</li>
-<li><a href="<?php print url('node/add/blastdb');?>">Create "Blast Database" nodes</a> for each dataset you want to make available for your users to BLAST against. BLAST databases should first be created using the command-line <code>makeblastdb</code> program with the <code>-parse_seqids</code> flag.</li>
-<li>It's recommended that you also install the <a href="http://drupal.org/project/tripal_daemon">Tripal Job Daemon</a> to manage BLAST jobs and ensure they are run soon after being submitted by the user. Without this additional module, administrators will have to execute the tripal jobs either manually or through use of cron jobs.</li>
+<li>Prepare pathway annotation file </li>
+The pathway annotation file is a tab delimit file with 4 columns: <br>
+<code>gene_id, gene_description(option), pathway_id, pathway_name </code>
+
+<li>Install perl module Statistics::Multtest</li>
+<code>sudo perl -MCPAN -e shell <br>
+  install Statistics::Multtest
+</code>
+
+<li>create soft link for pwy_tool.pl</li>
+Example: 
+<code>ln -s /var/www/html/sites/all/modules/tripal_pathway/pwy_tool.pl /usr/local/bin</code>
+
+<li><a href="<?php print url('node/add/pwy');?>">Create "Pathway File" nodes</a> for each dataset</li>
+	<ul>
+	<li>Human-readable name for pathway annotation file. For example, watermelon genome (97103) means the pathway annotation file is predicted using all genes of watermelon genome (97103).</li>
+	<li>Locateion of pathway annotation file, please use full path here. Example: /var/www/html/sites/all/modules/tripal_pathway/example/wm_97103_kept.pwy</li>
+    <li>The url link of gene ID. This link should not includes "http://" on the left, and must include back slash "/" if the original link to gene have one</li>
+    <li>The link of pathway ID. Same requirement as link to gene ID</li>
+    <li>Example of gene IDs. One ID for each line. These gene IDs should be exist in the pathway annotation file.</li>
+	</ul>
+<li>It's recommended that you also install the <a href="http://drupal.org/project/tripal_daemon">Tripal Job Daemon</a> to manage jobs and ensure they are run soon after being submitted by the user. Without this additional module, administrators will have to execute the tripal jobs either manually or through use of cron jobs.</li>
+
+<li>view <a href="<?php print url('pwyenrich');?>" >pathway enrichment</a> to submit a job.</li>
+
 </ol>
 
-<h3>Highlighted Functionality</h3>
-<ul>
-  <li>Supports <a href="<?php print url('blast/nucleotide/nucleotide');?>">blastn</a>, 
-    <a href="<?php print url('blast/nucleotide/protein');?>">blastx</a>, 
-    <a href="<?php print url('blast/protein/protein');?>">blastp</a> and 
-    <a href="<?php print url('blast/protein/nucleotide');?>">tblastx</a> with separate forms depending upon the database/query type.</li>
-  <li>Simple interface allowing users to paste or upload a query sequence and then select from available databases. Additionally, a FASTA file can be uploaded for use as a database to BLAST against (this functionality can be disabled).</li>
-  <li>Tabular Results listing with alignment information and multiple download formats (HTML, TSV, XML) available.</li>
-  <li>Completely integrated with <a href="<?php print url('admin/tripal/tripal_jobs');?>">Tripal Jobs</a> providing administrators with a way to track BLAST jobs and ensuring long running BLASTs will not cause page time-outs</li>
-  <li>BLAST databases are made available to the module by <a href="<?php print url('node/add/blastdb');?>">creating Drupal Pages</a> describing them. This allows administrators to <a href="<?php print url('admin/structure/types/manage/blastdb/fields');?>">use the Drupal Field API to add any information they want to these pages</a>.</li>
-  <li>BLAST database records can be linked to an external source with more information (ie: NCBI) per BLAST database.</li>
-</ul>
